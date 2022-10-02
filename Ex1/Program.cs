6014df8570bb9f744cd52052399b7b25fc50c7d4
Sasha P <apodlednevarab@gmail.com>
2022-10-02 13:53:38 +0300

    Ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" lol Hello, World!");
+﻿Console.WriteLine ("Введите 2 числа: ");
+int a = 0;
+int b = 0;
+
+string numberone = Console.ReadLine (); 
+string numbertwo = Console.ReadLine (); 
+
+a = int.Parse(numberone);
+b = int.Parse(numbertwo);
+
+if (a > b)
+{
+    Console.Write ($"max = {a}, min = {b} ");
+}
+else 
+{
+    Console.Write ($"max = {b}, min = {a}");
+}
